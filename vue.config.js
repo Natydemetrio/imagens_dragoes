2e17f34ac2341bec2e3cff4912518e28b8ff9bb2
Nathaly Gabrieli Goergen Demetrio <130701070+Natydemetrio@users.noreply.github.com>
2024-09-17 10:18:05 -0300

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,30 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  configureWebpack: async () => {
+    // Carrega módulos ESM dinamicamente
+    const imageminMozjpeg = (await import('imagemin-mozjpeg')).default;
+    const imageminWebp = (await import('imagemin-webp')).default;
+
+    return {
+      plugins: [
+        new ImageminPlugin({
+          pngquant: {
+            quality: '65-80', // Compressão de PNG
+          },
+          plugins: [
+            imageminMozjpeg({
+              quality: 75, // Compressão de JPEG
+            }),
+            imageminWebp({
+              quality: 75, // Conversão para WebP
+            }),
+          ],
+        }),
+      ],
+    };
+  },
+};
